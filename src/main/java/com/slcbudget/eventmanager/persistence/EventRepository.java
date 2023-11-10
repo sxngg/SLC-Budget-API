8489ad363bb12aecb00d639c8a6a0e581d488040
sxngg <santiagogarciagarzon.8@gmail.com>
2023-11-10 17:49:39 -0500

    feat: sentences problem solved, controllers optimized with projections, new endpoint implemented

--- src/main/java/com/slcbudget/eventmanager/persistence/EventRepository.java
+++ src/main/java/com/slcbudget/eventmanager/persistence/EventRepository.java
@@ -3,11 +3,25 @@ package com.slcbudget.eventmanager.persistence;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.slcbudget.eventmanager.domain.Event;
+import com.slcbudget.eventmanager.domain.projections.EventProjection;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
   
+  @Query("SELECT " + 
+    "e.event_id as event_id," +
+    "e.name as name," +
+    "e.description as description, "+ 
+    "e.type as type, "+ 
+    "e.picture as picture, "+ 
+    "e.owner.id as owner_id "+ 
+    "FROM Event e " +
+    "WHERE e.owner.id = :userId")
+  Page<EventProjection> findEventsByOwnerId(@Param("userId") Long userId, Pageable pageable);
+
   Page<Event> findByOwnerId(Long userId, Pageable pageable);
 
 }
